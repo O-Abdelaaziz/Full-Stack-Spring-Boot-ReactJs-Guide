0be587789e525a2fe78b45094c7f220464beed14
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-05-09 10:46:19 +0100

    Task 19: fetch data from database & update (books details component) components

--- 1-reactjs-crud-operations-hooks-axios/src/components/BookDetails/BookDetails.jsx
+++ 1-reactjs-crud-operations-hooks-axios/src/components/BookDetails/BookDetails.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import BookService from "../../services/BookService";
+import {useNavigate, useParams} from "react-router-dom";
 
 const BookDetails = () => {
-    return (
-        <div>
-            
+    const [book, setBook] = useState(null);
+    const { bookIsbn } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(bookIsbn){
+            getBookHandler(bookIsbn);
+        }
+    }, [bookIsbn]);
+
+    const getBookHandler = async (isbn) => {
+        await BookService.getBookByIsbn(isbn).then((response) => {
+            console.log(response.data);
+            setBook(response.data);
+        });
+    };
+    return <React.Fragment>
+        <div className="row">
+            <div className="col-md-4">
+                <img src="https://via.placeholder.com/400x600.png" className="img-fluid img-thumbnail" alt="book details" />
+            </div>
+
+            <div className="col-md-8">
+                <strong>ISBN</strong>
+                <p>{book?.isbn}</p>
+
+                <strong>Title</strong>
+                <p>{book?.title}</p>
+
+                <strong>Description</strong>
+                <p>{book?.description}</p>
+
+                <strong>Edition</strong>
+                <p>{book?.edition}</p>
+
+                <strong>Author</strong>
+                <p>{book?.author}</p>
+
+                <strong>Quantity</strong>
+                <p>{book?.quantity}</p>
+
+                <strong>Price</strong>
+                <p>{book?.price}</p>
+
+                <strong>Rating</strong>
+                <p>{book?.rating}</p>
+
+                <strong>Publish Date</strong>
+                <p>{book?.publishDate}</p>
+
+                <strong>Tags</strong>
+                <p>{book?.tags}</p>
+            </div>
         </div>
-    );
+    </React.Fragment>;
 };
 
 export default BookDetails;
