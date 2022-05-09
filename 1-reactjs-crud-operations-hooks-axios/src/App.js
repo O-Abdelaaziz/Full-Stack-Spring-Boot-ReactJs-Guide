a8d6ce4d0cff7cf962c5626e2e53d3c0ac0ced1d
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-05-09 09:23:59 +0100

    Task 12: update navbar, header, footer components

--- 1-reactjs-crud-operations-hooks-axios/src/App.js
+++ 1-reactjs-crud-operations-hooks-axios/src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import NavBar from "./components/Layout/NavBar";
+import Header from "./components/Layout/Header";
+import Footer from "./components/Layout/Footer";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+      <React.Fragment>
+        <NavBar />
+        <Header />
+        <Footer />
+      </React.Fragment>
   );
 }
 
