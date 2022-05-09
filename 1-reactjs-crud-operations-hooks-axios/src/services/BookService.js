cc51382ec2f075a478af45f3bd1f3240ac70ffef
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-05-09 09:28:38 +0100

    Task 14: update http-common & book service

---
+++ 1-reactjs-crud-operations-hooks-axios/src/services/BookService.js
@@ -0,0 +1,36 @@
+import http from "../http-common";
+
+const getAllBooks = () => {
+    return http.get("/books");
+};
+
+const getBookByIsbn = (isbn) => {
+    return http.get(`/books/${isbn}`);
+};
+
+const saveBook = (book) => {
+    return http.post("/books", book);
+};
+
+const updateBook = (isbn, book) => {
+    return http.put(`/books/${isbn}`, book);
+};
+
+const deleteBook = (isbn) => {
+    return http.delete(`/books/${isbn}`);
+};
+
+const deleteAllBooks = () => {
+    http.delete("/books");
+};
+
+const BookService = {
+    getAllBooks,
+    getBookByIsbn,
+    saveBook,
+    updateBook,
+    deleteBook,
+    deleteAllBooks,
+};
+
+export default BookService;
