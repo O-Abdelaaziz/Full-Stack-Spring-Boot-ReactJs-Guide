2924324cf0c15088585f513bf3d2fe9326b1d221
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-05-11 10:39:05 +0100

    Task 25: define book reducer & create default combine reducers

---
+++ 2-reactjs-crud-operations-hooks-redux/src/store/reducers/BookReducer.js
@@ -0,0 +1,47 @@
+import {
+    RETRIEVE_BOOKS,
+    RETRIEVE_BOOK_BY_ISBN,
+    CREATE_BOOK,
+    UPDATE_BOOK,
+    DELETE_BOOK,
+    DELETE_ALL_BOOKS,
+} from "../actions/Types";
+
+const initialState = {
+    books: [],
+    book: {}
+};
+
+const bookReducer = (state = initialState, action) => {
+    const {type, payload} = action;
+    switch (type) {
+        case RETRIEVE_BOOKS:
+            return {...state, books: payload};
+        case RETRIEVE_BOOK_BY_ISBN:
+            return {...state, book: payload};
+        case CREATE_BOOK:
+            return {...state, books: payload};
+        case UPDATE_BOOK:
+            return {
+                ...state, books: state.books.map((book) => {
+                    if (book.id === payload.id) {
+                        return {
+                            ...book,
+                            ...payload
+                        };
+                    } else {
+                        return book;
+                    }
+                })
+            };
+        case DELETE_BOOK:
+            return {...state, state: state.books.filter(({isbn}) => isbn !== payload.isbn)};
+        case DELETE_ALL_BOOKS:
+            return [];
+        default:
+            return state;
+    }
+}
+
+export default bookReducer;
+
