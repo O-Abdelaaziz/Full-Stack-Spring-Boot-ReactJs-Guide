034e8236c577c2af3f8fc0ed0cf04c08d10379d2
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-05-11 13:35:41 +0100

    Task 28: fetch & remove books from database using redux (dispatch)

--- 2-reactjs-crud-operations-hooks-redux/src/components/Books/BookItem.jsx
+++ 2-reactjs-crud-operations-hooks-redux/src/components/Books/BookItem.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import {deleteBookByIsbn, retrieveBooks} from "../../store/actions/BookActions";
+import {useDispatch} from "react-redux";
+import {Link} from "react-router-dom";
 
-const BookItem = () => {
+const BookItem = (props) => {
+    const {id, isbn, title, description, edition} = props.book;
+    const dispatch = useDispatch();
+
+    const removeBook = () => {
+        dispatch(deleteBookByIsbn(isbn))
+            .then(() => {
+                dispatch(retrieveBooks());
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    }
     return (
-        <div>
-            
-        </div>
+        <tr>
+            <td>{id}</td>
+            <td>{isbn}</td>
+            <td>{title}</td>
+            <td>{description}</td>
+            <td>{edition}</td>
+            <td>
+                <Link className="btn btn-success" to={"/books/" + isbn + "/edit"}>Edit</Link>
+                <button className={'btn btn-danger'} onClick={removeBook}>Remove</button>
+            </td>
+        </tr>
     );
 };
 
--- 2-reactjs-crud-operations-hooks-redux/src/components/Books/BooksList.jsx
+++ 2-reactjs-crud-operations-hooks-redux/src/components/Books/BooksList.jsx
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {deleteBooks, retrieveBooks} from "../../store/actions/BookActions";
+import BookItem from "./BookItem";
+import {toast, ToastContainer} from "react-toastify";
+import Section from "../UI/Section";
 
 const BooksList = () => {
+    const books = useSelector(state => state.books.books);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        dispatch(retrieveBooks());
+    }, []);
+
+    const dataTable = () => {
+        return books.map((res, i) => {
+            return <BookItem book={res} key={i}/>
+        })
+    }
+
+    const removeAllBooks = () => {
+        dispatch(deleteBooks())
+            .then(response => {
+                toast("All Books Was Deleted");
+                dispatch(retrieveBooks());
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    };
+
+    const onNavigate = () => {
+        navigate("/new-book", {replace: true});
+    };
+
     return (
-        <div>
-            
-        </div>
+        <Section>
+            <div className="container px-4 px-lg-5 mt-5">
+                <button className="btn btn-primary my-2" onClick={onNavigate}>Add new book</button>
+                <button className="btn btn-danger mx-2"  onClick={removeAllBooks}>Remove All</button>
+
+                <div className="row col-md-12 justify-content-center">
+                    <div className={'table-wrapper'}>
+                        <table className={'table table-striped table-bordered table-responsive table-hover'}>
+                            <thead className={'thead-dark'}>
+                            <tr>
+                                <th>ID</th>
+                                <th>ISBN</th>
+                                <th>Title</th>
+                                <th>Description</th>
+                                <th>Edition</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>{dataTable()}</tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <ToastContainer />
+        </Section>
     );
 };
 
